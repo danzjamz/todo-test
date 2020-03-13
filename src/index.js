775aeac38075a4f2c35d53df478698bb4cb62a2f
danzjamz <djl.hovley@gmail.com>
2020-03-12 20:50:57 -0600

    renamed app.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,69 @@
-import React from 'react';
-import App from './App';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Todos from './todos';
+import AddTodo from './add-todo';
+import GetTodos, { DeleteTodo } from './todoService';
+import './index.css';
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 
+class App extends Component {
+  state = {
+    todos: [ ]
+  }
+  
+  componentDidMount() {
+    GetTodos()
+    .then(res => {
+      console.log(res)
+      this.setState({ todos: res });
+    }).catch(err => {
+      console.log('get todos error ->', err)
+    });
+  }
+  
+  deleteTodo = (id) => {
+    DeleteTodo(id)
+    .then(() => {
+      this.setState({
+        todos: this.state.todos.filter(todo => {
+          return todo.id !== id
+        })
+      });
+    }).catch(err => {
+      console.log('delete todos error ->', err)
+    });
+  }
+  
+  addTodo = (todo) => {
+    todo.id = this.state.id;
+    this.setState({ id: this.state.id + 1 })
+    let todos = [ ...this.state.todos, todo ];
+    this.setState({ todos: todos })
+  }
+  
+  render() {
+    return (
+      <div className="app">
+        <h1>Todos</h1>
+        <AddTodo className='add-todo' addTodo={ this.addTodo } />
+        {/* <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo } /> */}
+
+        { this.state.todos ? (
+          this.state.todos.map(todo => {
+            return (
+              <Todos key={ todo.id } todo={ todo } deleteTodo={ this.deleteTodo } />
+              )
+            })
+            ) : (
+              <p className='center'>You have no todos</p>
+              )}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
