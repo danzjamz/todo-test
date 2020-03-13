ad9f5a83cc5116b99f6740780e4b0f7f4d906b07
danzjamz <djl.hovley@gmail.com>
2020-03-12 20:49:45 -0600

    moved render functionality to App.js file

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 import Todos from './todos';
 import AddTodo from './add-todo';
 import GetTodos, { DeleteTodo } from './todoService';
+import './index.css';
+
 
 
 class App extends Component {
   state = {
     todos: [ ]
   }
-
+  
   componentDidMount() {
     GetTodos()
-      .then(res => {
-        console.log(res)
-        this.setState({ todos: res });
-      }).catch(err => {
-        console.log('get todos error ->', err)
-      });
+    .then(res => {
+      console.log(res)
+      this.setState({ todos: res });
+    }).catch(err => {
+      console.log('get todos error ->', err)
+    });
   }
-
+  
   deleteTodo = (id) => {
     DeleteTodo(id)
-      .then(() => {
-        this.setState({
-          todos: this.state.todos.filter(todo => {
-              return todo.id !== id
-          })
-        });
-      }).catch(err => {
-        console.log('delete todos error ->', err)
+    .then(() => {
+      this.setState({
+        todos: this.state.todos.filter(todo => {
+          return todo.id !== id
+        })
       });
+    }).catch(err => {
+      console.log('delete todos error ->', err)
+    });
   }
-
+  
   addTodo = (todo) => {
     todo.id = this.state.id;
     this.setState({ id: this.state.id + 1 })
     let todos = [ ...this.state.todos, todo ];
     this.setState({ todos: todos })
   }
-
+  
   render() {
     return (
       <div className="app">
@@ -48,17 +51,19 @@ class App extends Component {
         {/* <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo } /> */}
 
         { this.state.todos ? (
-            this.state.todos.map(todo => {
-                return (
-                    <Todos key={ todo.id } todo={ todo } deleteTodo={ this.deleteTodo } />
-                )
+          this.state.todos.map(todo => {
+            return (
+              <Todos key={ todo.id } todo={ todo } deleteTodo={ this.deleteTodo } />
+              )
             })
-        ) : (
-            <p className='center'>You have no todos</p>
-        )}
+            ) : (
+              <p className='center'>You have no todos</p>
+              )}
       </div>
     );
   }
 }
 
 export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
